3f9e646cc6c7ceb3465b1362736c925230241377
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-10-26 02:48:41 +0700

    feat: update tvl for custom assets

--- services/interval-update/updateTVL.js
+++ services/interval-update/updateTVL.js
@@ -10,8 +10,9 @@ module.exports = async params => {
   // run every 15 minutes
   if (minute % 15 !== 0) return;
   const data = {};
+  await getTVL({ force_update: true, is_interval: true, custom_assets_only: true });
   for (const d of toArray(_.concat(await getAssetsList(), await getITSAssetsList())).filter(d => !params?.id || d.id === params.id).filter(d => params?.id || (minute % 30 === 0 ? d.id.startsWith('0x') : !d.id.startsWith('0x')))) {
-    data[d.id] = await getTVL({ asset: d.id, force_update: true });
+    data[d.id] = await getTVL({ asset: d.id, force_update: true, is_interval: true });
   }
   return data;
 };
