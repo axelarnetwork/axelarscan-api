cb93cca8e2031b84bd752ab68570bae463ac9433
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2025-02-14 17:45:11 +0800

    feat: update getTokenInfo to support query specific height

--- methods/axelar/getTokenInfo.js
+++ methods/axelar/getTokenInfo.js
@@ -4,15 +4,26 @@ const getTotalSupply = require('./getTotalSupply');
 const getCirculatingSupply = require('./getCirculatingSupply');
 const getTotalBurned = require('./getTotalBurned');
 const { getTokensPrice, getExchangeRates } = require('../tokens');
-const { CURRENCY, getAssetData, getITSAssetData } = require('../../utils/config');
+const { ENVIRONMENT, CURRENCY, getAssetData, getITSAssetData, getLCD } = require('../../utils/config');
+const { createInstance, request } = require('../../utils/http');
+
+const getBlockTimestamp = async height => {
+  if (!height) return;
+  const instance = createInstance(getLCD(ENVIRONMENT, true), { gzip: true });
+  const { block } = { ...await request(instance, { path: `/cosmos/base/tendermint/v1beta1/blocks/${height}` }) };
+  const { time } = { ...block?.header };
+  if (time) return moment(time).valueOf();
+  return;
+};
 
 module.exports = async params => {
   const { agent, height } = { ...params };
   let { symbol } = { ...params };
   symbol = symbol || 'AXL';
 
+  const timestamp = await getBlockTimestamp(height) || moment().valueOf();
   const { denom, name } = { ...(await getAssetData(symbol) || await getITSAssetData(symbol)) };
-  const { data, updated_at } = { ...await getTokensPrice({ symbol, currency: CURRENCY, debug: true }) };
+  const { data, updated_at } = { ...await getTokensPrice({ symbol, timestamp, currency: CURRENCY, debug: true }) };
   const { price } = { ...(data?.[symbol] || Object.values({ ...data }).find(d => d.denom === symbol)) };
   const supplyData = await getCirculatingSupply({ symbol, height, debug: true });
   const circulatingSupply = supplyData?.circulating_supply;
@@ -29,6 +40,6 @@ module.exports = async params => {
         return { symbol, currencyCode, price: _price, marketCap: circulatingSupply * _price, circulatingSupply, maxSupply: totalSupply, totalBurned, provider: 'Axelar', lastUpdatedTimestamp: updatedAt };
       });
     default:
-      return { symbol, name, price, marketCap: circulatingSupply * price, circulatingSupply, maxSupply: totalSupply, totalBurned, updatedAt };
+      return { symbol, name, price, marketCap: circulatingSupply * price, circulatingSupply, maxSupply: totalSupply, totalBurned, updatedAt, timestamp };
   }
 };
