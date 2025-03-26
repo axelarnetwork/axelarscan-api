584dade1820ef62d82bb93275abd5cedd7d84e2a
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2025-03-26 16:13:47 +0700

    chore: remove debug

--- services/interval-update/updateTVL.js
+++ services/interval-update/updateTVL.js
@@ -9,19 +9,13 @@ module.exports = async params => {
   const hour = moment().hours();
   const minute = moment().minutes();
 
-  console.log('checkpoint', 'updateTVL', { hour, minute })
-
   // get TVL of custom assets
   await getTVL({ forceCache: true, isIntervalUpdate: true, customAssetsOnly: true });
-
-  console.log('checkpoint', 'updateTVL after run custom assets', { hour, minute })
 
   const assetsData = _.concat(await getAssets(), await getITSAssets())
     .filter(d => !params?.id || d.id === params.id) // filter by params.id
     .filter(d => params?.id || (hour % 2 === 0 ? d.id.startsWith('0x') : !d.id.startsWith('0x'))) // run ITS on even hour, otherwise gateway
     .filter((d, i) => params?.id || parseInt(minute / 6) === i % 10); // seperate assets to run by minute into 10 groups
-
-  console.log('checkpoint', 'updateTVL', { hour, minute, ids: assetsData.map(d => d.id) })
 
   const data = {};
 
