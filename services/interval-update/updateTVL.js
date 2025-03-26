73c89beed577867329a480d9b0e73a63796046d9
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2025-03-26 15:11:40 +0700

    chore: update time and assets to run updateTVL

--- services/interval-update/updateTVL.js
+++ services/interval-update/updateTVL.js
@@ -3,25 +3,25 @@ const moment = require('moment');
 
 const { getTVL } = require('../../methods');
 const { getAssets, getITSAssets } = require('../../utils/config');
+const { log } = require('../../utils/logger');
 
 module.exports = async params => {
   const hour = moment().hours();
   const minute = moment().minutes();
-  // run every 15 minutes
-  if (minute % 15 !== 0) return;
 
   // get TVL of custom assets
   await getTVL({ forceCache: true, isIntervalUpdate: true, customAssetsOnly: true });
 
   const assetsData = _.concat(await getAssets(), await getITSAssets())
     .filter(d => !params?.id || d.id === params.id) // filter by params.id
-    .filter(d => params?.id || (minute % 30 === 0 ? d.id.startsWith('0x') : !d.id.startsWith('0x'))) // run ITS on min 0 and 30, otherwise gateway
-    .filter((d, i) => params?.id || hour % 3 === i % 3);
+    .filter(d => params?.id || (hour % 2 === 0 ? d.id.startsWith('0x') : !d.id.startsWith('0x'))) // run ITS on even hour, otherwise gateway
+    .filter((d, i) => params?.id || parseInt(minute / 6) === i % 10); // seperate assets to run by minute into 10 groups
 
   const data = {};
 
   for (const { id } of assetsData) {
     // get TVL of each asset
+    log('debug', 'axelarscan-api', 'updateTVL', { id, hour, minute });
     data[id] = await getTVL({ asset: id, forceCache: true, isIntervalUpdate: true });
   }
 
